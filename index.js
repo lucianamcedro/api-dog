acf44a4e817a50716bb1565f9e2299f9d56a3a22
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2022-10-14 17:04:48 -0300

    criação api-dogs

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const dogs = require("./src/dogs/dogs.json")
+
+const port = process.env.PORT || 3333
+
+app.get("/dogs", (req, res) => {
+    return res.json(dogs)
+})
+
+app.listen(port, () => {
+    console.log("Servidor está carregando...")
+})
