60afbdee1283f3ada83ee637eac1ccd0628b7cce
Luciana Monteiro Cedro <luciana_cedro@hotmail.com>
2022-12-02 11:20:53 -0300

    feature/incluindo-outra-rota

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const express = require("express")
 const app = express()
 const dogs = require("./src/dogs/dogs.json")
+const details = require("./src/dogs/details.json")
+    
 
 const port = process.env.PORT || 3333
 
 app.get("/dogs", (req, res) => {
     return res.json(dogs)
 })
-
+app.get("/details", (req, res) => {
+    return res.json(details)
+})
 app.listen(port, () => {
     console.log("Servidor está carregando...")
 })
